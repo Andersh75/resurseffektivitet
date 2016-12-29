9e16eb9863e0e49495fb6158ffcfc0146894311a
Andersh75 <andersh75@gmail.com>
2016-12-29 17:55:25 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -481,10 +481,10 @@ def background_process():
                 'result': "Please check the peer's state"
             })
 		else:
-		#return json.dumps([{value: 1, text: 'first'}, {value: 2, text: 'second'}], separators=(',', ':'))
-        return json.dumps({"c": 0, "b": 0})
+            return json.dumps({"c": 0, "b": 0})
 	except Exception as e:
 		return str(e)
+
 
 
 
