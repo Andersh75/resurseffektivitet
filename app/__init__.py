5f2f62bb9b479a192547117b792dd54a6934c14f
Andersh75 <andersh75@gmail.com>
2016-12-29 23:05:15 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -486,7 +486,8 @@ def index2():
 
 @app.route('/background_process', methods=['GET', 'POST'])
 def background_process():
-    return json.dumps({'value': 'OK'})
+    #return json.dumps({'value': 'OK'})
+    return json.dumps([{"value": 1, "text": "Hej"}, {"value": 2, "text": "Hopp"}], separators=(',', ':'))
 
 
 
