49716fa713b2940c36024e362c490ca30ac8191e
Andersh75 <andersh75@gmail.com>
2016-12-29 18:19:18 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -431,12 +431,11 @@ def index(page):
         #return render_template('base.html')
         #return render_template('test.xml'), mimetype='application/xml'
         print json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
-        print json.dumps([{'c': 0, 'b': 0}, {'a': 0, 'd': 0}], separators=(',', ':'))
+        print json.dumps([{value: 1, text: 'first'}, {value: 2, text: 'second'}], separators=(',', ':'))
         return render_template('blocks.html.j2', varia="TESTVARIABEL", varrubrik=xrubrik[0], xkurskod=xkurskod[0], courseid=page)
     xrubrik = db.session.query(Courses.code).filter(Courses.id == 17).first()
     xkurskod = db.session.query(Courses.name).filter(Courses.id == 17).first()
     return render_template('blocks.html.j2', varia="TESTVARIABEL", varrubrik=xrubrik[0], xkurskod=xkurskod[0], courseid=17)
-
 
 
 
