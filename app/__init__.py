271ac9926283446ed03f80328455b3da19c33627
Andersh75 <andersh75@gmail.com>
2016-12-30 16:55:34 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -123,6 +123,7 @@ teachers_schedules = db.Table('teachers_schedules',
 
 
 
+
 dates_classes = db.Table('dates_classes',
     db.Column('dates_id', db.Integer, db.ForeignKey('dates.id')),
     db.Column('classes_id', db.Integer, db.ForeignKey('classes.id'))
