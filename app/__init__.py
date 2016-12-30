b89a35c61f677cbac3a6ba0d7d0a11728e396941
Andersh75 <andersh75@gmail.com>
2016-12-30 16:20:19 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -28,7 +28,6 @@ def grabcoursepm(code):
 
 
 
-
     title = data['course']['title'][0]['#text']
     credits = data['course']['credits'][0]['#text']
     gradeScaleCode = data['course']['gradeScaleCode']['#text']
