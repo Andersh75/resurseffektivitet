1083a39decf7b9ebf715246fa3324e40328d1408
Andersh75 <andersh75@gmail.com>
2016-12-30 05:47:42 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -647,6 +647,7 @@ for item in xvar:
     print item
 
 
+
 xvar = db.session.query(Teachers.firstname, func.count(Classes.id)).join(Teachers.classes).group_by(Teachers.firstname).all()
 
 for item in xvar:
