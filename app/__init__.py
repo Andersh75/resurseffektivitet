66b5197dbb7db6c3c78b43b97711e0fad5eeea7e
Andersh75 <andersh75@gmail.com>
2017-01-03 00:26:15 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -218,7 +218,7 @@ class Schedules(db.Model):
 
 
 class RegistrationForm(Form):
-    username = TextField('Username', [validators.Length(min=4, max=20)])
+    username = TextField('Username', [validators.Length(min=1, max=20)])
     email = TextField('Email Address', [validators.Length(min=6, max=50)])
     password = PasswordField('New Password', [
         validators.Required(),
