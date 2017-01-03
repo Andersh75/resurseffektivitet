e0ce3919dbfa78cb82daeb179e7ca7fd9306a836
Andersh75 <andersh75@gmail.com>
2017-01-03 12:47:36 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -218,7 +218,7 @@ class Schedules(db.Model):
 
 class RegistrationForm(Form):
     username = TextField('Username', [validators.Length(min=2, max=20)])
-    initials = TextField('Initials', [validators.Length(min=2, max=20)])
+    firstname = TextField('First name', [validators.Length(min=2, max=20)])
     email = TextField('Email Address', [validators.Length(min=6, max=50)])
     password = PasswordField('New Password', [
         validators.Required(),
@@ -532,7 +532,7 @@ def register_page():
 
     if request.method == "POST" and form.validate():
             username  = form.username.data
-            initials  = form.initials.data
+            firstname  = form.firstname.data
             email = form.email.data
             password = form.password.data
             xrubrik = db.session.query(Teachers.initials).filter(Teachers.initials == username).first()
@@ -544,7 +544,7 @@ def register_page():
                 flash(username)
                 record = Teachers(**{
                     'initials' : username,
-                    'firstname' : initials,
+                    'firstname' : firstname,
                     'email' : email,
                     'password' : password
                 })
