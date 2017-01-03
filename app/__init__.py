406a9c8e1f0f253cf216b2920b416f361052d65d
Andersh75 <andersh75@gmail.com>
2017-01-03 13:09:56 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -220,7 +220,7 @@ class RegistrationForm(Form):
     initials = TextField('Initials', [validators.Length(min=2, max=20)])
     firstname = TextField('First name', [validators.Length(min=2, max=20)])
     lastname = TextField('Last name', [validators.Length(min=2, max=20)])
-    email = TextField('Email Address', [validators.Regexp(\w*kth.se\b)])
+    email = TextField('Email Address', [validators.Length(min=6, max=50)])
     password = PasswordField('New Password', [
         validators.Required(),
         validators.EqualTo('confirm', message='Passwords must match')
