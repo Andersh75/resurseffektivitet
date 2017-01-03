fad1a12d2fd0bc3d3accf2e96f28dcb4f52ae7c7
Andersh75 <andersh75@gmail.com>
2017-01-03 12:39:00 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -218,6 +218,7 @@ class Schedules(db.Model):
 
 class RegistrationForm(Form):
     username = TextField('Username', [validators.Length(min=2, max=20)])
+    initials = TextField('Initials', [validators.Length(min=2, max=20)])
     email = TextField('Email Address', [validators.Length(min=6, max=50)])
     password = PasswordField('New Password', [
         validators.Required(),
@@ -542,6 +543,7 @@ def register_page():
                 flash(username)
                 record = Teachers(**{
                     'initials' : username,
+                    'firstname' : initials
                     'email' : email,
                     'password' : password
                 })
