70ca9aaf3fc3cc6b1498221defa342448ea45f1c
Andersh75 <andersh75@gmail.com>
2017-01-04 16:55:53 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -287,10 +287,10 @@ def roomsPerDate(date):
         templist.append(item)
     return templist
 
-def AllDates():
+def AllDates(date):
 # Lista med salar som anvands i en kurs
     templist = []
-    tempvar = db.session.query(Dates).order_by(Dates.date).limit(10).all()
+    tempvar = db.session.query(Dates).order_by(Dates.date).limit(10).slice(page, page+1).all()
     for item in tempvar:
         templist.append(item)
     return templist
