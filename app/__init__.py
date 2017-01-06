2e548e455555949aed3676ebe1c88e8cc13c9d4b
Andersh75 <andersh75@gmail.com>
2017-01-06 14:27:17 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -24,5 +24,4 @@ req = urllib2.urlopen('http://www.kth.se/api/kopps/v1/course/HS1735')
 
 xml = BeautifulSoup(req)
 
-for item in xml.examiner['primaryEmail']:
-    print (item)
+print xml.examiner.attrs
