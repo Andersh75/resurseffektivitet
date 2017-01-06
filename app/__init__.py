8a937dca81cd95a476ad04a151412c277c78c98d
Andersh75 <andersh75@gmail.com>
2017-01-06 17:37:52 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -26,58 +26,58 @@ app = Flask(__name__)
 
 
 def jsonifycoursesfromdepartment():
-j = urllib2.urlopen('http://www.kth.se/api/kopps/v2/courses/AIB.json')
+    j = urllib2.urlopen('http://www.kth.se/api/kopps/v2/courses/AIB.json')
 
-j_obj = json.load(j)
+    j_obj = json.load(j)
 
-templist = []
+    templist = []
 
-for item in j_obj['courses']:
-    #print item['code']
-    templist.append(item['code'])
+    for item in j_obj['courses']:
+        #print item['code']
+        templist.append(item['code'])
 
-templist2 = []
-tempdict = {}
+    templist2 = []
+    tempdict = {}
 
-for item in templist:
-    req = urllib2.urlopen('http://www.kth.se/api/kopps/v1/course/%s' % (item))
+    for item in templist:
+        req = urllib2.urlopen('http://www.kth.se/api/kopps/v1/course/%s' % (item))
 
-    xml = BeautifulSoup(req)
-
-
-    #vartitle = xml.title.string
-    try:
-        varcode = xml.course['code']
-        print varcode
-
-    except Exception, e:
-        varcode = "no title"
-        print varcode
+        xml = BeautifulSoup(req)
 
 
-    try:
-        varmail = xml.examiner['primaryemail']
-        print varmail
+        #vartitle = xml.title.string
+        try:
+            varcode = xml.course['code']
+            print varcode
 
-    except Exception, e:
-        varmail = "no mail"
-        print varmail
+        except Exception, e:
+            varcode = "no title"
+            print varcode
 
 
-    try:
-        vartitle = xml.title.string
-        #print vartitle.encode('utf-8')
-        print vartitle
+        try:
+            varmail = xml.examiner['primaryemail']
+            print varmail
 
-    except Exception, e:
-        vartitle = "no title"
-        print vartitle
+        except Exception, e:
+            varmail = "no mail"
+            print varmail
 
-    tempdict = {'code':varcode, 'title':vartitle, 'examiner':varmail}
 
-    templist2.append(tempdict)
+        try:
+            vartitle = xml.title.string
+            #print vartitle.encode('utf-8')
+            print vartitle
 
-return templist2
+        except Exception, e:
+            vartitle = "no title"
+            print vartitle
+
+        tempdict = {'code':varcode, 'title':vartitle, 'examiner':varmail}
+
+        templist2.append(tempdict)
+
+    return templist2
 
 
 
