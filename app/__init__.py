500c02cd30717b46d9059ae6951c63e9fa1b0a0e
Andersh75 <andersh75@gmail.com>
2017-01-06 15:45:46 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -56,7 +56,7 @@ def hello_world():
     #print tempdict
 
 
-    return jsonify(code=varcode, examiner=varmail, title=(xml.title.string).encode('utf-8'))
+    return jsonify(code=varcode, examiner=varmail, title=vartitle)
 
 
 
