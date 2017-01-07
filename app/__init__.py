ec201c004c82a02c3d6464c66c41e2fe5d6dbeec
Andersh75 <andersh75@gmail.com>
2017-01-07 15:14:24 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -354,7 +354,7 @@ def hello_world():
     varcourse.responsible_id = 10
     db.session.commit()
 
-    print db.session.query(Courses.responsible).first()
+    print db.session.query(Courses.firstname).first()
 
     return "testx"
 
