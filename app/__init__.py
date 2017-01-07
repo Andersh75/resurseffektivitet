fb1b073145f53caf5804df1f7947595f33b3fba6
Andersh75 <andersh75@gmail.com>
2017-01-07 12:58:54 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -273,7 +273,7 @@ def calTest():
     req = urllib2.Request('https://www.kth.se/social/course/AI1147/calendar/ical/?lang=sv')
     response = urllib2.urlopen(req)
     data = response.read()
-    pint data
+    print data
 
     return
 
