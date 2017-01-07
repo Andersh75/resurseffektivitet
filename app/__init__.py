23782dede2e584d9f949c39087074a06cb05f0d2
Andersh75 <andersh75@gmail.com>
2017-01-07 03:11:18 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -193,7 +193,7 @@ def coursesfromdepartment(templist):
             tempdict = {}
             exists = db.session.query(Courses).filter(code == code).count()
             print exists
-            if title and code and (examiner != "no mail") and department and (exists > 0):
+            if title and code and (examiner != "no mail") and department and (exists == 0):
                 tempdict['title'] = title
                 tempdict['code'] = code
                 tempdict['examiner'] = examiner
