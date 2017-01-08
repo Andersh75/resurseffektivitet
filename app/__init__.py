272fbeb32e8de8f76d1d3cea7cdae9819b9ee8ce
Andersh75 <andersh75@gmail.com>
2017-01-08 16:59:38 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -120,6 +120,20 @@ class Classes(db.Model):
     dates_id = db.Column(db.Integer, db.ForeignKey('dates.id'))
 
 
+class RegistrationForm(Form):
+    initials = TextField('Initials', [validators.Length(min=2, max=20)])
+    firstname = TextField('First name', [validators.Length(min=2, max=20)])
+    lastname = TextField('Last name', [validators.Length(min=2, max=20)])
+    email = TextField('Email Address', [validators.Length(min=9, max=17)])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    #accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice (updated Jan 22, 2015)', [validators.Required()])
+
+
+
 ### CREATE TABLES
 def createtables():
     db.create_all()
