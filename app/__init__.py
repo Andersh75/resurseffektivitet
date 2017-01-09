1013f29ebf4c34d0d066af53f6727f2569cb59fe
Andersh75 <andersh75@gmail.com>
2017-01-09 14:39:15 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -199,7 +199,7 @@ def amiresponsible(code):
 
 def amiteaching(code):
     testvar = db.session.query(Teachers).join(Teachers.classes).join(Classes.courses).filter(and_(Teachers.email==session['user'], Courses.code==code)).first()
-    print testvar
+    print testvar.code
     already = False
     if testvar:
         already = True
