66e52133c78f5b2509704ac94d6d376d8eb79120
Andersh75 <andersh75@gmail.com>
2017-01-09 02:23:04 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -154,9 +154,9 @@ def mycourseslist():
     id = 1
     for item in testvar[:-1]:
         print item
-        tempvar = tempvar + " id: " + str(id) + ", text: '" + item[0] + "' }, "
+        tempvar = tempvar + " id: " + str(id) + ", text: '" + item[0] + "' }, {"
         id = id + 1
-    tempvar = tempvar + "{ id: " + str(id) + ", text: '" + testvar[-1][0] + "' }]"
+    tempvar = tempvar + " id: " + str(id) + ", text: '" + testvar[-1][0] + "' }]"
 
     print tempvar
     tempvar2 = "[{ id: 1, text: 'bug' }, { id: 2, text: 'duplicate' }, { id: 3, text: 'invalid' }, { id: 4, text: 'wontfix' }]"
