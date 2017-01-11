65afda00e0ec1d56b6153a31885086935f932792
Andersh75 <andersh75@gmail.com>
2017-01-12 00:06:05 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -375,6 +375,7 @@ def teachersonslot(slotid):
 
 
 
+
 def subjectsinslot(slotid):
     templist = db.session.query(Subjects).join(Subjects.classes).filter(Classes.id == slotid).all()
     for item in templist:
