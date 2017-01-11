02e753b1f28568a76e07aa6930b09d4f911b11a8
Andersh75 <andersh75@gmail.com>
2017-01-11 22:07:55 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -1147,7 +1147,6 @@ def allteachers_page(page=1):
     return render_template('allteachers.html.j2', page=page)
 
 
-
 @app.route('/oneteacher')
 @app.route('/oneteacher/<int:teacherid>')
 def oneteacher_page(teacherid=1):
