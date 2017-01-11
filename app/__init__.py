620481043c48f106cdf5c5c73deda5734f234dc8
Andersh75 <andersh75@gmail.com>
2017-01-11 21:21:52 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -291,9 +291,9 @@ def myobject():
 def mycoursesexaminerorresponsible():
     examiner = aliased(Teachers)
     responsible = aliased(Teachers)
-    #templist = db.session.query(Courses).join(examiner, Courses.examiner).join(responsible, Courses.responsible).filter(or_(examiner.email == session['user'], responsible.email == session['user'])).all()
-    templist = db.session.query(Courses).join(examiner, Courses.examiner).join(responsible, Courses.responsible).all()
-    print templist
+    templist = db.session.query(Courses).join(examiner, Courses.examiner).join(responsible, Courses.responsible).filter(or_(examiner.email == session['user'], responsible.email == session['user'])).all()
+    #templist = db.session.query(Courses).join(examiner, Courses.examiner).join(responsible, Courses.responsible).all()
+    #print templist
     return templist
 
 def mycoursesresponsible():
