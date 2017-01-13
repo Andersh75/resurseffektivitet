811a8a0481fa8451934f6f258259a86d8da99fdb
Andersh75 <andersh75@gmail.com>
2017-01-13 19:48:07 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -1332,8 +1332,8 @@ def testlogin():
         USERNAME = 'ahell'
         PASSWORD = '-Gre75kger-'
         c.get(url)
-        logindata = dict(username=USERNAME, password=PASSWORD)
-        c.post(url, data=logindata, headers={"Referer": "https://www.kth.se/"})
+        logdata = dict(username=USERNAME, password=PASSWORD)
+        c.post(url, data=logdata, headers={"Referer": "https://www.kth.se/"})
         page = c.get('https://www.kth.se/internt/minasidor/kurs/delt/?ccode=AI1147&term=V17')
 
     return page.content
