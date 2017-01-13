298000558498744dc80b8c7940667911115720e8
Andersh75 <andersh75@gmail.com>
2017-01-13 23:40:12 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -993,7 +993,7 @@ def coursesfromdepartment2(item):
         tempdict['endweek'] = endweek
         #tempdict['responsible_id'] = Teachers.query.filter_by(email=responsible).first().id
         print "BEFOR ENDWEEK"
-        record = Courses(**tempdict)
+        record = Courses(tempdict)
         print "ENDWEEK"
         print record.endweek
         db.session.add(record)
