c41bfb1d8055fa6dd5ebcda32e6afe22da6f5f51
Andersh75 <andersh75@gmail.com>
2017-01-14 03:28:01 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -1576,7 +1576,7 @@ def testlogin():
     br.set_handle_refresh( mechanize._http.HTTPRefreshProcessor(), max_time = 1 )
 
     br.addheaders = [ ( 'User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1' ) ]
-
+    print "heh"
     # authenticate
     br.open( 'https://login.kth.se/login/' )
 
