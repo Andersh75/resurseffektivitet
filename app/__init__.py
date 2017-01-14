b0ad99f6cc074b97bda3eb179cc4ba1d4871f80e
Andersh75 <andersh75@gmail.com>
2017-01-14 21:19:08 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -34,6 +34,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.by import By
 
 
