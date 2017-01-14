d165900978b9d30648cc28762a488533d4f69b01
Andersh75 <andersh75@gmail.com>
2017-01-14 01:27:02 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -1618,7 +1618,7 @@ def restartall():
 
     departments = ["AIB", "AIC", "AID", "AIE"]
 
-
+    '''
     for item in departments:
 
         tempdict2 = staffperdepartment(item)
@@ -1628,24 +1628,23 @@ def restartall():
     #ADD ALL TEACHERS TO DB
     teachersfromdepartment(templist2)
 
-
-
-
-
     '''
+
+
+
+
 
     tempdict20151 = courseinfoperyearandterm(2015, 1)
-    tempdict20152 = courseinfoperyearandterm(2015, 2)
-    tempdict20161 = courseinfoperyearandterm(2016, 1)
-    tempdict20162 = courseinfoperyearandterm(2016, 2)
-    tempdict20171 = courseinfoperyearandterm(2017, 1)
+    #tempdict20152 = courseinfoperyearandterm(2015, 2)
+    #tempdict20161 = courseinfoperyearandterm(2016, 1)
+    #tempdict20162 = courseinfoperyearandterm(2016, 2)
+    #tempdict20171 = courseinfoperyearandterm(2017, 1)
 
     addcoursestotables_first(tempdict20151)
-    addcoursestotables_first(tempdict20152)
-    addcoursestotables_first(tempdict20161)
-    addcoursestotables_first(tempdict20162)
-    addcoursestotables_first(tempdict20171)
-    '''
+    #addcoursestotables_first(tempdict20152)
+    #addcoursestotables_first(tempdict20161)
+    #addcoursestotables_first(tempdict20162)
+    #addcoursestotables_first(tempdict20171)
     '''
     for item in departments:
         #print item
