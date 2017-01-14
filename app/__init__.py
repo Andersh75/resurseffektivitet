08cd26d0bf21d9247f19c1f130931b110bd5c679
Andersh75 <andersh75@gmail.com>
2017-01-14 19:38:55 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -1816,7 +1816,7 @@ def testlogin():
     return "Hopp"
 
 
-
+#Collects links to all slots in history
 @app.route('/testscrape')
 def testscrape():
 
@@ -1983,6 +1983,68 @@ def testslots():
 
     return "HO"
 
+
+@app.route('/fetchslotfromsociallink')
+def fetchslotfromsociallink():
+
+
+    # Browser
+    br = mechanize.Browser()
+
+    # Enable cookie support for urllib2
+    cookiejar = cookielib.LWPCookieJar()
+    br.set_cookiejar( cookiejar )
+
+    # Broser options
+    br.set_handle_equiv( True )
+    br.set_handle_gzip( True )
+    br.set_handle_redirect( True )
+    br.set_handle_referer( True )
+    br.set_handle_robots( False )
+
+    # ??
+    br.set_handle_refresh( mechanize._http.HTTPRefreshProcessor(), max_time = 1 )
+
+    br.addheaders = [ ( 'User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1' ) ]
+    print "heh"
+    # authenticate
+    br.open( 'https://login.kth.se/login/' )
+
+    br.select_form(nr=0)
+    # these two come from the code you posted
+    # where you would normally put in your username and password
+    br[ "username" ] = 'ahell'
+    br[ "password" ] = '-Gre75kger-'
+    res = br.submit()
+
+    print "Success!\n"
+
+
+    testlink = "https://www.kth.se"
+
+    testlink = testlink + "/social/course/HS1700/subgroup/vt-2011-50454/event/ovning-2011-02-23-1000-17/"
+
+    try:
+        url = br.open(testlink)
+
+    except Exception, e:
+        varcode = "no social"
+        print varcode
+        print item.code
+
+
+    try:
+        xml = BeautifulSoup(url)
+    except Exception, e:
+        varcode = "no BS"
+        print varcode
+
+    print xml.find('a', itemprop=lambda value: value and value.startswith(startDate))
+
+
+
+
+    return "HO"
 
 
 @app.route('/restartall')
