993e1624d08a26193edf4f24ccebef36aa738b52
Andersh75 <andersh75@gmail.com>
2017-01-15 12:23:07 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -2118,7 +2118,7 @@ def create_or_fetch_roomobj(roomvar):
 def create_or_fetch_classobj(starttimevar, endtimevar, codevar, yearvar, datevar, roomobj):
 
     classobj = db.session.query(Classes).join(Classes.courses).join(Classes.rooms).join(Classes.dates).filter(and_(Courses.code==codevar, Rooms.name==roomobj.name, Dates.date==dateobj.date, Classes.starttime==starttimevar, Classes.endtime==endtimevar)).first()
-
+    print classobj
     if not classobj:
         print "CREATING CLASSOBJECT"
         tempdict = {}
