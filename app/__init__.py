4ee2ec4d621b0c0b1ddbe7e4a5135d9eaffb1adb
Andersh75 <andersh75@gmail.com>
2017-01-15 12:28:33 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -2116,7 +2116,7 @@ def create_or_fetch_roomobj(roomvar):
     return roomobj
 
 def create_or_fetch_classobj(starttimevar, endtimevar, codevar, yearvar, datevar, roomobj):
-
+    print "createorfetchclassobj"
     if roomobj:
         classobj = db.session.query(Classes).join(Classes.courses).join(Classes.rooms).join(Classes.dates).filter(and_(Courses.code==codevar, Rooms.name==roomobj.name, Dates.date==dateobj.date, Classes.starttime==starttimevar, Classes.endtime==endtimevar)).first()
     else:
