06fe8cfec4a5c6fb8920e5ffec34315aa5121bb7
Andersh75 <andersh75@gmail.com>
2017-01-15 15:36:34 +0100

    s

--- app/__init__.py
+++ app/__init__.py
@@ -2067,7 +2067,7 @@ def testlogin():
 @app.route('/linkstoeveryclassinsocial')
 def linkstoeveryclassinsocial():
 
-    open_password_protected_site("https://login.kth.se/login/")
+    #open_password_protected_site("https://login.kth.se/login/")
 
     linklist = []
     br = mechanize.Browser()
@@ -2087,6 +2087,18 @@ def linkstoeveryclassinsocial():
     br.set_handle_refresh( mechanize._http.HTTPRefreshProcessor(), max_time = 1 )
 
     br.addheaders = [ ( 'User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1' ) ]
+
+    br.open("https://login.kth.se/login/")
+
+    br.select_form(nr=0)
+    # these two come from the code you posted
+    # where you would normally put in your username and password
+    br[ "username" ] = 'ahell'
+    br[ "password" ] = '-Gre75kger-'
+    res = br.submit()
+
+    print "Success!\n"
+
 
 
     for item in allcourses():
