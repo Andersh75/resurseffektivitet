c2b905501def1829e006157e703201c4a3f7f217
Andersh75 <andersh75@gmail.com>
2017-01-24 19:20:38 +0100

    S

--- app/__init__.py
+++ app/__init__.py
@@ -1958,7 +1958,7 @@ def slotsfromsocial():
     # testcourse = ["AI2808"]
 
     templist = allcourses()
-    templist = templist[:100]
+    templist = templist[:50]
     for idx, item in enumerate(templist):
         # for idx, item in enumerate(testcourse):
         coursecode = item.code
