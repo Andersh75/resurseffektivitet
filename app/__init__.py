dcd74a6f653fcec24ad961af28f788e68dfcb4ed
Andersh75 <andersh75@gmail.com>
2017-01-26 23:09:40 +0100

    S

--- app/__init__.py
+++ app/__init__.py
@@ -963,6 +963,7 @@ def create_or_fetch_courseobj(code, year):
 
         if not courseobj:
             print "CREATING_COURSEOBJECT"
+            print db.session.query(Courses).filter(and_(Courses.code == code, Courses.year == year)).first()
             tempdict = {}
             tempdict['code'] = code
             tempdict['year'] = year
