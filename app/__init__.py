9c2bc8ea01ee2c08980be1b439bc095412723cdb
Andersh75 <andersh75@gmail.com>
2017-01-28 20:39:08 +0100

    S

--- app/__init__.py
+++ app/__init__.py
@@ -1974,7 +1974,7 @@ def editslot():
             for item in teacherlist:
                 print item
 
-            teacherobj = db.session.query(Teachers).filter(Teacher.id == item).first()
+            teacherobj = db.session.query(Teachers).filter(Teachers.id == item).first()
             print teacherobj.email
             classobj = db.session.query(Classes).filter(Classes.id == idvar).first()
             print classobj.id
