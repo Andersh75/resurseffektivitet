ac7d8dc6d73874013cec7af5748eaf13dc02010b
Andersh75 <andersh75@gmail.com>
2017-01-28 16:50:08 +0100

    S

--- app/__init__.py
+++ app/__init__.py
@@ -2033,7 +2033,6 @@ def fetchregistredandexpectedstudents():
 
 
 
-
 # Adding slots from schedule API for all courses
 def slotsfromscheduleapi(coursecode):
         tempdict = fetchinglistofslotspercourse(coursecode, "2011-01-01", "2018-06-30")
