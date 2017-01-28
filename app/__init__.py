339b81c26b4dad1901c2e8382a77113df2d70992
Andersh75 <andersh75@gmail.com>
2017-01-28 16:53:12 +0100

    S

--- app/__init__.py
+++ app/__init__.py
@@ -2038,6 +2038,7 @@ def slotsfromscheduleapi(coursecode):
         parselistofslotspercourse(tempdict)
 
 
+
 # Adding slots from Social for all courses
 @app.route('/slotsfromapiandsocial')
 def slotsfromsocial():
