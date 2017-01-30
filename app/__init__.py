f46a35d58ce3f2ebcd5e8fe8675c368765f856b1
Andersh75 <andersh75@gmail.com>
2017-01-30 19:03:42 +0100

    S

--- app/__init__.py
+++ app/__init__.py
@@ -539,13 +539,13 @@ def teacherslistjson():
 def teacherslistjson2():
     templist = db.session.query(Teachers.email).all()
 
-    tempvar = "{"
+    tempvar = '{'
     id = 1
     for item in templist[:-1]:
         # print item
-        tempvar = tempvar + " '"'id'"': " + str(id) + ", '"'text'"': '" + item[0] + "' }, {"
+        tempvar = tempvar + ' "'"id"'": "' + str(id) + '", "'"text"'": "' + item[0] + '" }, {'
         id = id + 1
-    tempvar = tempvar + " '"'id'"': '" + str(id) + "', '"'text'"': '" + templist[-1][0] + "' }"
+    tempvar = tempvar + ' "'"id"'": "' + str(id) + '", "'"text"'": "' + templist[-1][0] + '" }'
 
     # print tempvar
     # tempvar2 = "[{ id: 1, text: 'bug' }, { id: 2, text: 'duplicate' }, { id: 3, text: 'invalid' }, { id: 4, text: 'wontfix' }]"
