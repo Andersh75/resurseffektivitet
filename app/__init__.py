14d19fe22bb300ff9a4d4c01ef255d632e8b658a
Andersh75 <andersh75@gmail.com>
2017-02-06 02:43:54 +0100

    S

--- app/__init__.py
+++ app/__init__.py
@@ -2959,3 +2959,5 @@ if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=1080)
     #app.run(host='0.0.0.0', port=1080)
     #app.run(host='dansfolk.no-ip.org', port=1080)
+
+    
