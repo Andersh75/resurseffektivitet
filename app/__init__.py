10ba19c3b1f39f5cc51cc007323fe1463fa4a554
Andersh75 <andersh75@gmail.com>
2017-02-06 02:25:28 +0100

    S

--- app/__init__.py
+++ app/__init__.py
@@ -2956,5 +2956,5 @@ def Xslotsfromsocial():
 
 if __name__ == "__main__":
     app.secret_key = 'asdasdasdasdasd'
-    app.run(debug=True, host='0.0.0.0', port=1080)
-    # app.run(host='0.0.0.0', port=1080)
+    # app.run(debug=True, host='0.0.0.0', port=1080)
+    app.run(host='0.0.0.0', port=1080)
