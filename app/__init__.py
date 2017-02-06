93bf57c51fa3028dc32831483951684194624594
Andersh75 <andersh75@gmail.com>
2017-02-06 02:01:08 +0100

    S

--- app/__init__.py
+++ app/__init__.py
@@ -2660,6 +2660,7 @@ def printabookedroom():
 
         datevar = str(item.startdate)
         datevar = datevar[:10]
+        print datevar
         if datevar == "2016-11-03":
             print str(item.lastchangeddate) + ": " + items[1] + " " + str(item.startdate) + " " + str(item.starttime) + "-" + str(item.endtime) + " >>> " + str(item.status)
 
